d12d1ac14ec7c94c2ce669ead2f9f5aebd45bfb2
Puji Rahmadiyanto <puji.rahmadiyanto@gmail.com>
2011-12-09 19:58:26 +0700

    tampil

--- application/models/m_berita.php
+++ application/models/m_berita.php
@@ -7,5 +7,12 @@ class M_berita extends CI_Model{
     function save($data){
         $this->db->insert("berita",$data);
     }
+    function tampil(){
+
+        $result = $this->db->get("berita");
+        if($result->num_rows() >0){
+            return $result->result_array();
+        }
+    }
 }
 ?>
